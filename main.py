7224bab8244703005a9efe30fdd87f60bb20a7cf
Hugo Fernández Solís <hugofernandezsolis@gmail.com>
2022-08-18 20:37:43 +0100

    Initialized main function

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import streamlit as st
+import tensorflow as tf
+
+# Initializes the enviroment and controlls the flow
+def main() -> None:
+  print("Main function")
+
+# Checks we are running the app in the main function.
+if __name__ == "__main__":
+  main()
