892aa857fde198f19162aed699442758810493da
Hugo Fernández Solís <hugofernandezsolis@gmail.com>
2022-08-18 21:14:53 +0100

    Changed github action

--- tests/ModelGenerator_tests.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from ModelGenerator import *
-
-def test_create_datasets() -> None:
-  trainingDataset, validationDataset = create_datasets("datasets/train")
-  assert(trainingDataset != None)
-  assert(validationDataset != None)
