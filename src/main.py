6df4445a2de7fce0a76b9d5ae5294c8765f34aad
Hugo Fernández Solís <hugofernandezsolis@gmail.com>
2022-08-19 15:33:03 +0100

    Added haar cascade models

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
 import streamlit as st
 import tensorflow as tf
+import ModelGenerator as mg
 
 
 # Initializes the enviroment and controlls the flow
 def main() -> None:
-    print("Main function")
+    face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
+    leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
+    reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+    model: tf.keras.models.Sequential = mg.create_model("datasets/eyes/train")
 
 
 # Checks we are running the app in the main function.
