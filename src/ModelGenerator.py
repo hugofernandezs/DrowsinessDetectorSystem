0fab9dff7374998ae77fac06efe29b1045434afe
Hugo Fernández Solís <hugofernandezsolis@gmail.com>
2022-09-07 10:26:06 +0100

    Added face model

--- src/ModelGenerator.py
+++ src/ModelGenerator.py
@@ -7,46 +7,25 @@ import tensorflow as tf
 # Some constants.
 TARGET_SIZE: tuple[int, int] = (24, 24)
 COLOR_MODE: str = "grayscale"
+ACCURACY: float = 0.95
 IMAGE_SIZE: int = 24
 BATCH_SIZE: int = 32
+EPOCHS: int = 99999
 
 # Remove Tensorflow annoying messages.
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
-# Creates the training and validation datasets from the passed dir.
-def create_datasets(dir: str) -> tuple[tf.data.Dataset, tf.data.Dataset]:
-    trainingDataset: tf.data.Dataset = tf.keras.preprocessing.image_dataset_from_directory(
-        dir,
-        color_mode=COLOR_MODE,
-        batch_size=BATCH_SIZE,
-        image_size=(IMAGE_SIZE, IMAGE_SIZE),
-        seed=0,
-        validation_split=0.2,
-        subset="training"
-    )
-    validationDataset: tf.data.Dataset = tf.keras.preprocessing.image_dataset_from_directory(
-        dir,
-        color_mode=COLOR_MODE,
-        batch_size=BATCH_SIZE,
-        image_size=(IMAGE_SIZE, IMAGE_SIZE),
-        seed=0,
-        validation_split=0.2,
-        subset="validation"
-    )
-    trainingDataset: tf.data.Dataset = trainingDataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-    validationDataset: tf.data.Dataset = validationDataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-    return trainingDataset, validationDataset
-
-def generator(dir, gen=tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
-
-    return gen.flow_from_directory(dir,batch_size=batch_size,shuffle=shuffle,color_mode='grayscale',class_mode=class_mode,target_size=target_size)
+def generator(dir, shuffle=True, batch_size=1, target_size=TARGET_SIZE,
+              class_mode='categorical'):
+    return tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)\
+            .flow_from_directory(dir, batch_size=batch_size, shuffle=shuffle,
+            color_mode=COLOR_MODE, class_mode=class_mode, target_size=target_size)
 
 
 # Creates and fits a convolutional neural network.
-def create_model(dir: str, accuracy: int = 0.95, activation: str = "relu", optimizer: str = "adam",
-                 epochs: int = 10) -> tf.keras.models.Sequential:
-    modelName: str = str(f"{dir.split('/')[-2]}_{activation}_{optimizer}_CNN.h5")
+def create_model(dir: str) -> tf.keras.models.Sequential:
+    modelName: str = str(f"{dir.split('/')[-1]}CNN.h5")
     # If the model exists, it loads it and returns it.
     if os.path.exists(f"models/{modelName}"):
         print(f"{modelName} -> Loaded")
@@ -58,40 +37,40 @@ def create_model(dir: str, accuracy: int = 0.95, activation: str = "relu", optim
             super(Callback, self).__init__()
 
         def on_epoch_end(self, epoch, logs=None):
-            if logs["val_accuracy"] >= accuracy and logs["accuracy"] >= accuracy:
+            if logs["val_accuracy"] >= ACCURACY and logs["accuracy"] >= ACCURACY:
                 self.model.stop_training = True
     # Creates the model.
     model: tf.keras.models.Sequential = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1), activation="relu"),
-        tf.keras.layers.MaxPooling2D(pool_size=(1, 1)),
-        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(pool_size=(1, 1)),
+        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), input_shape=\
+        (IMAGE_SIZE, IMAGE_SIZE, 1), activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(pool_size=(1, 1)),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Dropout(rate=0.5),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(units=128, activation="relu"),
-        tf.keras.layers.Dropout(rate=0.5),
-        # Usamos la activación sigmoid porque es la más correcta para salidas binarias.
-        tf.keras.layers.Dense(units=2, activation="sigmoid")
+        tf.keras.layers.Dropout(rate=0.2),
+        tf.keras.layers.Dense(units=2, activation="softmax")
     ])
     model.compile(
         optimizer='adam',
         loss='binary_crossentropy',
         metrics=["accuracy"]
     )
-    with open(os.devnull, "w") as f, contextlib.redirect_stdout(f):
-        trainingDataset, validationDataset = create_datasets(dir)
-    train_batch= generator('datasets/eyes/train',shuffle=True, batch_size=32,target_size=(24,24))
-    valid_batch= generator('datasets/eyes/test',shuffle=True, batch_size=32,target_size=(24,24))
+
+    trainBatch = generator(f"{dir}/train", shuffle=True, batch_size=32, target_size=(24,24))
+    validBatch = generator(f"{dir}/test", shuffle=True, batch_size=32, target_size=(24,24))
 
     model.fit(
-        train_batch,
-        validation_data=valid_batch,
-        epochs=epochs,
+        trainBatch,
+        validation_data=validBatch,
+        epochs=EPOCHS,
         callbacks=[Callback()],
         shuffle = True
     )
+    
     print(f"{modelName} -> Created")
     model.save(f"models/{modelName}")
     return model
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 import cv2 as cv
 import numpy as np
 import streamlit as st
@@ -11,6 +10,16 @@ def exit_sys():
     cap.release() 
     st.stop()
 
+# Algunas constantes del programa
+frameCount: int = 0
+# warningScore: int = 10
+
+# maxScore: int = 100
+# scoreIncrease: int = 1
+
+# maxThicc: int = 10
+# thiccIncrease: int = 2
+
 # Opens thw webcam for capturing images.
 cap: cv.VideoCapture = cv.VideoCapture(0)
 
@@ -21,69 +30,137 @@ rightEyeCascade = cv.CascadeClassifier(f"{cv.data.haarcascades}haarcascade_right
 
 # Prepare de streamlit widgets.
 st.title("Drowsiness Detection System")
-col1, col2 = st.columns([2, 1])
+col1, col2 = st.columns([2, 2])
 imagePlaceHolder = col1.empty()
 exitButton: st.button = st.button(label="Exit", on_click=exit_sys)
 st.sidebar.title("User Inputs")
 
+eyesTagText = col2.empty()
+faceTagText = col2.empty()
+labelText = col2.empty()
+frameCountText = col2.empty()
+
+warningHolder = st.empty()
+
+# Algunas constantes del programa
+warningScore = st.sidebar.slider("Warning score:", min_value=0, max_value=100, value=10)
+
+maxScore = st.sidebar.slider("Max score", min_value=0, max_value=100, value=100)
+scoreIncrease = st.sidebar.slider("Score increase", min_value=0, max_value=10, value=1)
+scoreDecrease = st.sidebar.slider("Score decrease", min_value=0, max_value=10, value=1)
+
+maxThicc = st.sidebar.slider("Max thiccness", min_value=0, max_value=100, value=10)
+thiccIncrease = st.sidebar.slider("Thiccness increase", min_value=0, max_value=10, value=2)
+thiccDecrease = st.sidebar.slider("Thiccness decrease", min_value=0, max_value=10, value=2)
+
 # Initializes the enviroment and controlls the flow.
 def main() -> None:
     # Trains or loads the model.
-    model: tf.keras.models.Sequential = mg.create_model("datasets/eyes/train")
-    label: str = "None"
+    eyesModel: tf.keras.models.Sequential = mg.create_model("datasets/eyes")
+    faceModel: tf.keras.models.Sequential = mg.create_model("datasets/yawn")
     score: int = 0
     thicc: int = 0
-    leftEye: None = None
-    rightEye: None = None
+    frameCount: int = 0
     # Keeps reading while the cap is opened.
+
+    print("\nStarting detection...\n")
     while(cap.isOpened()):
+        label: str = ""
+        eyesTag: str = ""
+        yawnTag: str = ""
+
         ret, frame = cap.read()
+
         if ret is True:
+            frameCount += 1
+            leftEye: np.ndarray = np.ndarray([])
+            rightEye: np.ndarray = np.ndarray([])
+            face: np.ndarray = np.ndarray([])
+
             # Detect the face and both eyes.
-            frame: np.ndarray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            faceShape: np.ndarray = faceCascade.detectMultiScale(frame, minNeighbors=5, scaleFactor=1.1, minSize=(25,25))
-            leftEyeShape: np.ndarray = leftEyeCascade.detectMultiScale(frame, minNeighbors=4, scaleFactor=1.1)
-            rightEyeShape: np.ndarray =  rightEyeCascade.detectMultiScale(frame, minNeighbors=4, scaleFactor=1.1)
+            frame: np.ndarray = cv.flip(frame, 1)
+            grayFrame: np.ndarray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            faceShape: np.ndarray = faceCascade.detectMultiScale(grayFrame, minNeighbors=5, scaleFactor=1.1, minSize=(25,25))
+            leftEyeShape: np.ndarray = leftEyeCascade.detectMultiScale(grayFrame, minNeighbors=4, scaleFactor=1.1)
+            rightEyeShape: np.ndarray =  rightEyeCascade.detectMultiScale(grayFrame, minNeighbors=4, scaleFactor=1.1)
+            
             # Print rectangles on face and eyes for visualizing purposes.
             for (x, y, w, h) in faceShape:
-                cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
+                face: np.ndarray = grayFrame[y:y+h,x:x+w]
+                face: np.ndarray = cv.resize(face, (mg.IMAGE_SIZE, mg.IMAGE_SIZE))
+                face: np.ndarray = face / 255
+                face: np.ndarray = face.reshape(mg.IMAGE_SIZE, mg.IMAGE_SIZE, -1)
+                face: np.ndarray = np.expand_dims(face,axis=0)
+                face: np.ndarray = faceModel.predict(face, verbose=0)[0]
+                cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
             for (x, y, w, h) in leftEyeShape:
-                leftEye: np.ndarray = frame[y:y+h,x:x+w]
+                leftEye: np.ndarray = grayFrame[y:y+h,x:x+w]
                 leftEye: np.ndarray = cv.resize(leftEye, (mg.IMAGE_SIZE, mg.IMAGE_SIZE))
                 leftEye: np.ndarray = leftEye / 255
                 leftEye: np.ndarray = leftEye.reshape(mg.IMAGE_SIZE, mg.IMAGE_SIZE, -1)
                 leftEye: np.ndarray = np.expand_dims(leftEye,axis=0)
-                leftEye: np.ndarray = model.predict(leftEye, verbose=0)[0]
-                cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
+                leftEye: np.ndarray = eyesModel.predict(leftEye, verbose=0)[0]
+                # cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
             for (x, y, w, h) in rightEyeShape:
-                rightEye: np.ndarray = frame[y:y+h,x:x+w]
+                rightEye: np.ndarray = grayFrame[y:y+h,x:x+w]
                 rightEye: np.ndarray = cv.resize(rightEye, (mg.IMAGE_SIZE, mg.IMAGE_SIZE))
                 rightEye: np.ndarray = rightEye / 255
                 rightEye: np.ndarray = rightEye.reshape(mg.IMAGE_SIZE, mg.IMAGE_SIZE, -1)
                 rightEye: np.ndarray = np.expand_dims(rightEye,axis=0)
-                rightEye: np.ndarray = model.predict(rightEye, verbose=0)[0]
-                cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
-            if leftEye[0] < 0.5 or rightEye[0] < 0.5:
-                label: str = "Open"
-                if score > 0:
-                    score -= 1
-                if thicc > 0:
-                    thicc -= 2
-            else:
-                label: str = "Close"
-                score += 1
-                if score > 10 and thicc < 10:
-                    thicc += 2
-            cv.putText(frame, label, (10, frame.shape[0] - 20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255),
-                      1, cv.LINE_AA)
-            cv.rectangle(frame, (0,0), (frame.shape[1]-1, frame.shape[0]-1), (0,0,255), thicc)
-            cv.putText(frame, 'Score:'+str(score), (100, frame.shape[0]-20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
-                      (255, 255, 255), 1, cv.LINE_AA)
-            imagePlaceHolder.image(frame, caption='Video')
+                rightEye: np.ndarray = eyesModel.predict(rightEye, verbose=0)[0]
+                # cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            
+            if rightEye is None or leftEye is None or face is None:
+                warningHolder.warning("Error on processing the image!")
+                label: str = "ERROR ON IMAGE!!"
 
-        if cv.waitKey(1) == ord('q'):
+            else:
+                try:
+                    # Analizamos los ojos.
+                    if leftEye[0] < 0.5 or rightEye[0] < 0.5:
+                        eyesTag: str = "Open"
+                    else:
+                        eyesTag: str = "Close"
+                    
+                    # Analizamos el rostro.
+                    if face[0] < 0.5:
+                        yawnTag: str = "No"
+                    else:
+                        yawnTag: str = "Yes"
+                    
+                    # Analizamos el conjunto.
+                    if yawnTag == "No" and eyesTag == "Open":
+                        if score > 0:
+                            score -= scoreDecrease
+                        if thicc > 0:
+                            thicc -= thiccDecrease
+                        label: str = "Awake"
+                    else:
+                        if score < maxScore:
+                            score += scoreIncrease
+                        if score > warningScore and thicc < maxThicc:
+                            thicc += thiccIncrease
+                        label: str = "Sleep"
+
+                    # Mostramos los resultados.
+                    cv.putText(frame, label, (10, frame.shape[0] - 20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255),
+                            1, cv.LINE_AA)
+                    cv.rectangle(frame, (0,0), (frame.shape[1]-1, frame.shape[0]-1), (0,0,255), thicc)
+                    cv.putText(frame, 'Score:'+str(score), (100, frame.shape[0]-20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1,
+                            (255, 255, 255), 1, cv.LINE_AA)
+                    imagePlaceHolder.image(cv.cvtColor(frame, cv.COLOR_BGR2RGB), caption='Video')
+                
+                except:
+                    warningHolder.warning("Please, try better ilumination!")
+
+                eyesTagText.text(f"Eyes prediction: {eyesTag}")
+                faceTagText.text(f"Yawn prediction: {yawnTag}")
+                labelText.text(f"Drowsiness prediction: {label}")
+                frameCountText.text(f"Frames predicted: {frameCount}")
+        
+        if cv.waitKey(500) == ord('q'):
             cap.release()
             cv.destroyAllWindows()
 
